d045f48d6bad79208e892a3024e649aad97d713a
Susanna Kayed <kefka.sk@gmail.com>
2024-02-19 16:05:44 +0100

    first count down mechanism added

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(WORK_MIN*60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def countdown(count):
-    canvas.itemconfig(timer_text)
+    
+    minutes = math.floor(count/60)
+    seconds= count%60
+    if seconds < 10:
+        seconds =f"0{seconds}"
+    
+
+    canvas.itemconfig(timer_text,text=f'{minutes}:{seconds}')
     if count > 0:
         window.after(1000, countdown, count-1)
 
@@ -33,10 +43,12 @@ canvas.create_image(100,112, image = tomato)
 timer_text = canvas.create_text(100,132, text="00:00", fill="white", font=(FONT_NAME, 30) )
 canvas.grid(column=1,row=1)
 
+
+
 reset_button = Button(text="reset", highlightthickness=0)
 reset_button.grid(column=2, row=3)
 
-start_button = Button(text="start", highlightthickness=0)
+start_button = Button(text="start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=3)
 
 check_text = Label(text="✓", fg=GREEN, bg=YELLOW, font=(20))
