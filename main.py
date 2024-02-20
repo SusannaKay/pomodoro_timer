6e13c00ae9f0b999dd19ebcc833916671ae6c159
Susanna Kayed <kefka.sk@gmail.com>
2024-02-20 06:49:24 +0100

    added reset button function

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 5
-SHORT_BREAK_MIN = 3
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 txt = ""
-
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+def reset_timer():
+    window.after_cancel(timer)
+    title_label.config(text="Timer")
+    canvas.itemconfig(timer_text, text="00:00")
+    check_text.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
@@ -20,13 +29,13 @@ def start_timer():
 
     reps+=1
     if reps%8 == 0:
-        countdown(LONG_BREAK_MIN)
+        countdown(LONG_BREAK_MIN *60)
         title_label.config(text="Break",fg=RED)
     elif reps%2 == 0:
-        countdown(SHORT_BREAK_MIN)
+        countdown(SHORT_BREAK_MIN *60)
         title_label.config(text="Break",fg=PINK)
     else:
-        countdown(WORK_MIN)
+        countdown(WORK_MIN *60)
         title_label.config(text="Work",fg=GREEN)
     
     
@@ -35,6 +44,7 @@ def start_timer():
 def countdown(count):
     global reps
     global txt
+    global timer
     
     minutes = math.floor(count/60)
     seconds= count%60
@@ -44,7 +54,7 @@ def countdown(count):
 
     canvas.itemconfig(timer_text,text=f'{minutes}:{seconds}')
     if count > 0:
-        window.after(1000, countdown, count-1)
+        timer = window.after(1000, countdown, count-1)
     else:
         
         if reps %2 == 0:
@@ -69,7 +79,7 @@ canvas.grid(column=1,row=1)
 
 
 
-reset_button = Button(text="reset", highlightthickness=0)
+reset_button = Button(text="reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=3)
 
 start_button = Button(text="start", highlightthickness=0, command=start_timer)
